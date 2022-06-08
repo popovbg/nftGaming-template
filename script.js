eb4667f4862c394582bb90adbc30655c416e2875
popovbg <89911454+popovbg@users.noreply.github.com>
2022-06-08 14:12:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+	function showNav(){
+		var nav = document.getElementById("res-nav");
+		if (nav.style.display === "none") {
+			nav.style.display = 'flex';
+		}else {
+			nav.style.display = 'none';
+		}
+	}
